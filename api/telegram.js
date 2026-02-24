c9305fae4c04dffbf542a4ca9476feb9a2345dc7
Moatasem Alhilali <m.alhilalee@gmail.com>
2026-02-24 14:55:04 +0300

    d

--- api/telegram.js
+++ api/telegram.js
@@ -165,7 +165,9 @@ export default async function handler(req, res) {
       return;
     }
 
-    const downloadedMedia = await downloadLinkedInMedia(mediaTargets);
+    const downloadedMedia = await downloadLinkedInMedia(mediaTargets, {
+      referer: post.preferredReferer,
+    });
     if (downloadedMedia.length === 0) {
       res.status(200).json({ ok: true });
       return;
